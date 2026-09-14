086569ff40855d78083df2140f12f46fd4a6e99f
Guru-playmaster <gurusubramanianp@gmail.com>
2026-09-14 18:46:37 +0530

    changed version

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 def home():
     return """
     <h1>My CI/CD Project</h1>
-    <p>Hello! My first DevOps application is running.</p>
-    <p>Version: 1.1</p>
+    <p>Hello! My first DevOps application is running.Guru</p>
+    <p>Version: 1.2</p>
     """
 
 
