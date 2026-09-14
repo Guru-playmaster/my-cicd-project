4711536bbea0718ba04482d8dd3c76f9b8c48168
Guru-playmaster <gurusubramanianp@gmail.com>
2026-09-14 19:49:13 +0530

    updated app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def home():
     return """
     <h1>My CI/CD Project</h1>
     <p>Hello! My first DevOps application is running.Guru kasi</p>
-    <p>Version: 1.2</p>
+    <p>Version: 2.0</p>
     """
 
 
